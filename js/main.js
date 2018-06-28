6d7ebea0709929b7859c193aa0a841cc3457a7e7
titanmind <sharpnova@gmail.com>
2018-06-27 22:47:43 -0500

    div testing

--- js/main.js
+++ js/main.js
@@ -1,45 +1,9 @@
-var tutorialText1 = "Your vision begins to adjust, you can make out two faint shapes nearby. They seem to be moving.";
-var buttonText1 = "Inspect First Shape";
-var tutorialText2 = "You can make out a deeply slumbering Queen who seems to be well below the activation threshold for any activity.";
-var buttonText2 = "Inspect Second Shape";
-var tutorialText3 = "A lone, purposeless worker, starved, slow. But it will be enough.";
-var buttonText3 = "Send Worker to Explore";
-var tutorialText4 = "The worker feels around for gaps in the walls, it finds none.";
-var buttonText4 = "Explore some More";
-var tutorialText5 = "";
-var buttonText5 = "";
-
-var buttonState = 0;
-
-
 $(document).ready(function() {
-
-	function changeTutorialText(contentText,buttonText) {
-		$("#content").fadeOut(100,function() {
-				$(this).text(contentText).fadeIn(100);	
-			});
-		$("#button1").fadeOut(100,function() {
-				$(this).text(buttonText).fadeIn(100);
-			});
-		buttonState++;
+	function changeContentText(newText) {
+		$("#content").fadeOut(function(){$(this).text(newText).fadeIn()});
 	}
 
-    $("#button1").click(function() {
-    	if (buttonState==0) {
-	  		changeTutorialText(tutorialText1,buttonText1);
-		}
-		else if (buttonState==1) {
-			changeTutorialText(tutorialText2,buttonText2);
-		}
-		else if (buttonState==2) {
-			changeTutorialText(tutorialText3,buttonText3);
-		}
-		else if (buttonState==3) {
-			changeTutorialText(tutorialText4,buttonText4);	
-		}
-		else if (buttonState==4) {
-			changeTutorialText(tutorialText5,buttonText5);
-		}
-	});
-});
+	changeContentText("An ant queen slumbers here. She's in a deep sleep, well below the energy threshold for activity. Her breathing steady and slow. A lone, purposeless worker idles nearby.");
 
+	$("#content").html("hi");
+});
