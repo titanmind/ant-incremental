8801e419f4520a6861a06e535239a953248d4261
titanmind <sharpnova@gmail.com>
2018-06-29 01:08:35 -0500

    recursive callback for intro sequence

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+
 $(document).ready(function() {
-	function changeContentText(newText) {
-		$("#content").fadeOut(function(){$(this).text(newText).fadeIn()});
+	var introMessages = [];
+	introMessages.push("An ant queen slumbers here. She's in a deep sleep, well below the energy threshold for activity. Her breathing steady and slow.");
+	introMessages.push("<br>A lone, purposeless worker idles nearby.")
+
+	//function changeContentText(newText) {
+	//	$("#content").fadeOut(function(){$(this).text(newText).fadeIn()});
+	//}
+
+	function appendContentText(newText, callbackFunction, callbackArgument) {
+		$("<div>" + newText + "</div>").hide().appendTo("#content").fadeIn(1000, function() {
+			callbackFunction(callbackArgument)
+		});
 	}
 
-	changeContentText("An ant queen slumbers here. She's in a deep sleep, well below the energy threshold for activity. Her breathing steady and slow. A lone, purposeless worker idles nearby.");
+	function runIntroSequence(introCounter = 0) {
+		if (introCounter == introMessages.length) return;
+		else appendContentText(introMessages[introCounter], runIntroSequence, introCounter + 1);
+	}
 
-	$("#content").promise().done(function(){$("#content").append("test")});
+	runIntroSequence();
 });
