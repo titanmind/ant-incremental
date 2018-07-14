c7aee7d18e4f45d57aa1889767208210adfceb5c
titanmind <sharpnova@gmail.com>
2018-07-14 02:47:00 -0500

    refactored intro sequence

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 
 $(document).ready(function() {
-	var introMessages = [];
-	introMessages.push("An ant queen slumbers here. She's in a deep sleep, well below the energy threshold for activity. Her breathing steady and slow.");
-	introMessages.push("<br>A lone, purposeless worker idles nearby.")
-
-	//function changeContentText(newText) {
-	//	$("#content").fadeOut(function(){$(this).text(newText).fadeIn()});
-	//}
-
-	function appendContentText(newText, callbackFunction, callbackArgument) {
-		$("<div>" + newText + "</div>").hide().appendTo("#content").fadeIn(1000, function() {
-			callbackFunction(callbackArgument)
-		});
+	function appendText(text, delay){
+		setTimeout(
+			function(){
+				$("<div>" + "<br>" + text + "</div>").hide().appendTo("#content").fadeIn(1000);}, 4500);
 	}
 
-	function runIntroSequence(introCounter = 0) {
-		if (introCounter == introMessages.length) return;
-		else appendContentText(introMessages[introCounter], runIntroSequence, introCounter + 1);
+	function runIntroSequence(){
+		$("<div>" + "An ant queen slumbers here. She's in a deep sleep, well below the energy threshold for activity. Her breathing steady and slow." + "</div>").hide().appendTo("#content").fadeIn(2000);
+		appendText("A lone, purposeless worker idles nearby.", 4500);
+		setTimeout(
+			function(){
+				$("<br><input type='button' value='Send worker to explore' id='exploreButton'>").hide().appendTo("#content").fadeIn(2000);
+				document.getElementById('exploreButton').onclick = function(){
+					alert(3);};
+			}
+			, 8000);
 	}
 
 	runIntroSequence();
